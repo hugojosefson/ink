8dab24b7955a27f07491213d9a4bc799dc561a29
Hugo Josefson <hugo@josefson.org>
2021-11-28 14:26:59 +0100

    Delete all except src/ and license.

--- test/fixtures/exit-normally.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {Text, render} from '../..';
-
-const {waitUntilExit} = render(<Text>Hello World</Text>);
-waitUntilExit().then(() => console.log('exited'));
--- test/fixtures/exit-with-thrown-error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '../..';
-
-const Test = () => {
-	throw new Error('errored');
-};
-
-const app = render(<Test />);
-app.waitUntilExit().catch(error => console.log(error.message));
